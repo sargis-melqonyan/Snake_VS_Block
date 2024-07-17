e2e21c810a3d70bbdcb4bbf4f94dfd84c4d6979f
sargis-melqonyan <melqonyans500@gmail.com>
2024-07-17 10:40:41 +0400

    added: cmake

---
+++ build/CMakeFiles/Snake_VS_Block.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake_VS_Block.dir/src/ball.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/ball.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/block.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/block.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/board.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/board.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/controller.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/controller.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/game.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/game.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/main.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/main.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/mainMenu.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/mainMenu.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/snake.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/snake.cpp.o.d"
+  "CMakeFiles/Snake_VS_Block.dir/src/view.cpp.o"
+  "CMakeFiles/Snake_VS_Block.dir/src/view.cpp.o.d"
+  "Snake_VS_Block"
+  "Snake_VS_Block.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake_VS_Block.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
